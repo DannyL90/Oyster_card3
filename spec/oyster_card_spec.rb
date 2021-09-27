8c4e4018bf78f4e76c45253adb8240f0b9e07ff3
Daniel <daniel.szabo09@gmail.com>
2021-09-27 16:18:51 +0100

    Added top_up method

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -5,4 +5,11 @@ describe OysterCard do
   it "Oyster card default balance" do
     expect(subject.balance).to eq(0)
   end
+
+  it "Top up the card" do
+    subject.top_up(10)
+    expect(subject.balance).to eq(10)
+  end
+
+
 end
