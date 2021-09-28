632b2c99ba77522c6d74386acb56ccabbcdc92be
Daniel <daniel.szabo09@gmail.com>
2021-09-28 16:11:53 +0100

    Added minimum limit to touch_in method

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -3,11 +3,13 @@ class OysterCard
     attr_reader :balance, :in_use
     attr_accessor :limit
     DEFAULT_LIMIT = 90
+    MINIMUM_LIMIT = 1
 
     def initialize
         @balance = 0
 				@in_use = false
         @limit = DEFAULT_LIMIT
+        @minimum_limit = MINIMUM_LIMIT
     end
 
     def top_up(value)
@@ -24,14 +26,15 @@ class OysterCard
     end
 
     def touch_in
-			@in_use = true
+      fail "Not enough fund" if @balance < @minimum_limit
+		  @in_use = true
     end
 
-		def touch_out
-			@in_use = false
+	  def touch_out
+		  @in_use = false
     end
 
-		def in_journey?
-			@in_use
-		end
+	  def in_journey?
+		  @in_use
+	  end
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -6,18 +6,20 @@ describe OysterCard do
     expect(subject.balance).to eq(0)
   end
 
-  it "Top up the card" do
-    # subject.top_up(10)
-    # expect(subject.balance).to eq(10)
-    expect{ subject.top_up 1 }.to change { subject.balance }.by 1
-  end
+  describe "#top_up" do
+    it "Top up the card" do
+      # subject.top_up(10)
+      # expect(subject.balance).to eq(10)
+      expect{ subject.top_up 1 }.to change { subject.balance }.by 1
+    end
 
-  it "raises an error when the balace reaches £90" do
-    # subject.top_up(90)
-    # expect { subject.top_up 1 }.to raise_error "The maximum limit of £90 is reached"
-    maximum_balance = OysterCard::DEFAULT_LIMIT
-    subject.top_up(maximum_balance)
-    expect{ subject.top_up 1 }.to raise_error "The maximum limit of £#{maximum_balance} is reached"
+    it "raises an error when the balace reaches £90" do
+      # subject.top_up(90)
+      # expect { subject.top_up 1 }.to raise_error "The maximum limit of £90 is reached"
+      maximum_balance = OysterCard::DEFAULT_LIMIT
+      subject.top_up(maximum_balance)
+      expect{ subject.top_up 1 }.to raise_error "The maximum limit of £#{maximum_balance} is reached"
+    end
   end
 
   it "deduct amount from a balance" do
@@ -25,16 +27,25 @@ describe OysterCard do
     expect{ subject.deduct 1 }.to change { subject.balance }.by -1
   end
 
-  it "touches in oyster card" do
-    expect(subject.touch_in).to eq true
+  describe "#touch_in" do
+    it "touches in oyster card" do
+      subject.top_up(10)
+      expect(subject.touch_in).to eq true
+    end
+
+    it "raises error when your balance less then £1" do
+      expect{ subject.touch_in }.to raise_error "Not enough fund"
+    end
   end
 
   it "touches out oyster card" do
+    subject.top_up(10)
     subject.touch_in
     expect(subject.touch_out).to eq false
   end
 
   it "return true if 'In use'" do
+    subject.top_up(10)
     subject.touch_in
     expect(subject.in_journey?).to eq true
   end
