d6a5b09bd83bc5542593068a0549354f527d61d9
Daniel <daniel.szabo09@gmail.com>
2021-09-29 11:47:54 +0100

    Refactored spec file with before

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -20,44 +20,47 @@ describe OysterCard do
       subject.top_up(maximum_balance)
       expect{ subject.top_up 1 }.to raise_error "The maximum limit of £#{maximum_balance} is reached"
     end
-  end
-
-  describe "#touch_in" do
-    it "touches in oyster card" do
-      subject.top_up 10
-      subject.touch_in station
-      expect(subject.in_journey?).to eq true
-    end
 
     it "raises error when your balance less than £1" do
       expect { subject.touch_in(station) }.to raise_error "Not enough fund"
     end
 
-    it "remembers the entry station" do
+  end
+
+  describe "#touch_in and touch_out" do
+    before do
       subject.top_up 10
-      subject.touch_in(station)
-      expect(subject.entry_station).to eq station
-    end
-  end
-
-  describe "#touch_out" do
-    it "touches out oyster card" do
-      subject.top_up 10
-      subject.touch_in(station)
-      expect(subject.touch_out).to eq nil
     end
 
-    it "deducts balance by £1 when touch out" do
-      subject.top_up(10)
-      expect { subject.touch_out }.to change { subject.balance }.by -1
-    end
+      describe "#touch_in" do
 
+        it "touches in oyster card" do
+          subject.touch_in station
+          expect(subject.in_journey?).to eq true
+        end
+
+        it "remembers the entry station" do
+          subject.touch_in(station)
+          expect(subject.entry_station).to eq station
+        end
+      end
+
+      describe "#touch_out" do
+
+        it "touches out oyster card" do
+          subject.touch_in(station)
+          expect(subject.touch_out).to eq nil
+        end
+
+        it "deducts balance by £1 when touch out" do
+          expect { subject.touch_out }.to change { subject.balance }.by -1
+        end
+
+        it "return true if 'In use'" do
+          subject.touch_in(station)
+          expect(subject.in_journey?).to eq true
+        end
+
+      end
   end
-
-  it "return true if 'In use'" do
-    subject.top_up(10)
-    subject.touch_in(station)
-    expect(subject.in_journey?).to eq true
-  end
-
 end
